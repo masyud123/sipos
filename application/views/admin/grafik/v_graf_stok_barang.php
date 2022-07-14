24b34046235538842754d30c827b0a139ee24209
masyud123 <ahmadtya676@gmail.com>
2022-07-14 17:32:12 +0700

    print grafik

--- application/views/admin/grafik/v_graf_stok_barang.php
+++ application/views/admin/grafik/v_graf_stok_barang.php
@@ -6,6 +6,34 @@
     <!-- load library jquery dan highcharts -->
     
     <!-- end load library -->
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url().'assets/css/bootstrap.min.css'?>" rel="stylesheet">
+    <link href="<?php echo base_url().'assets/css/style.css'?>" rel="stylesheet">
+    <link href="<?php echo base_url().'assets/css/font-awesome.css'?>" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url().'assets/css/4-col-portfolio.css'?>" rel="stylesheet">
+    <link href="<?php echo base_url().'assets/css/dataTables.bootstrap.min.css'?>" rel="stylesheet">
+    <link href="<?php echo base_url().'assets/css/jquery.dataTables.min.css'?>" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap-datetimepicker.min.css'?>">
+    <link href="<?php echo base_url().'assets/dist/css/bootstrap-select.css'?>" rel="stylesheet">
+
+    <style>
+        @media print {
+            body * {
+                visibility: hidden;
+            }
+
+            #report, #report * {
+                visibility: visible;
+            }
+            
+            #report {
+                position: absolute;
+                left: 0;
+                top: 0;
+            }
+        }
+    </style>
 </head>
 <body>
 
@@ -22,8 +50,20 @@
 <!-- Load chart dengan menggunakan ID -->
 <div id="report"></div>
 <!-- END load chart -->
- 
 
+<div style="margin-left: 30px;">
+    <div class="d-flex justify-content-start">
+        <a class="btn btn-warning mr-5" id="kembali" onclick="window.location.href='<?=base_url('admin/Grafik')?>'">KEMBALI</a>
+        <a class="btn btn-primary ml-5" onclick="window.print()" id="cetak">CETAK</a>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="<?php echo base_url().'assets/js/jquery.js'?>"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url().'assets/dist/js/bootstrap-select.min.js'?>"></script>
+<script src="<?php echo base_url().'assets/js/bootstrap.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/grafik/jquery.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/grafik/highcharts.js'?>"></script>
 <!-- Script untuk memanggil library Highcharts -->
